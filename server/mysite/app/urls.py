53995a7a76c5ec3871b5584fa27d942832e58bad
amitbenski <amitbenski@gmail.com>
2017-05-18 16:00:52 +0300

    working server with homepage and another random page

--- server/mysite/app/urls.py
+++ server/mysite/app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-import views
+from app import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='home'), # Notice the URL has been named
-    url(r'^about/$', views.SomePageView.as_view(), name='somepage'),
+    url(r'^somepage/$', views.SomePageView.as_view(), name='somepage'),
 ]
--- server/mysite/app/views.py
+++ server/mysite/app/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+class SomePageView(TemplateView):
+    template_name = "somepage.html"
